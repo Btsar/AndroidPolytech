5e01ebf1a3e5d8f0e3af4d311357dc34691601cf
MARCELLIN Betsara <betsara.marcellin@etu.unice.fr>
2018-05-20 14:08:23 +0200

    Feature : implementation facebook et gmail en cours (opti mail)

--- app/src/main/java/etu/polytech/gip/MainActivity.java
+++ app/src/main/java/etu/polytech/gip/MainActivity.java
@@ -7,10 +7,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     //UI reference
     EditText mEmail, mPassword;
-    Button signIn, signOut;
+    Button signIn, signOut, signUp;
 
     @Override
     protected void onStart() {
@@ -41,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         signIn = (Button) findViewById(R.id.email_sign_in_button);
+        signUp = (Button) findViewById(R.id.email_sign_up_button);
         signOut = (Button) findViewById(R.id.email_sign_out_button);
 
 
@@ -82,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 toastTxt("Déconnexion en cours ...");
             }
         });
+
 
     }
 
